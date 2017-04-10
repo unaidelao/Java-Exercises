4b8d887fafcb0b2e06d55383f291c310eed87e2b
unaidelao <unai.delao@gmail.com>
2017-04-10 15:32:57 +0200

    Add Ejercicio 1.3

---
+++ java2-SchaumSeries/cap01/Ejercicio1_3.java
@@ -0,0 +1,26 @@
+/**
+ * Programación en Java 2, Serie Schaum
+ *
+ * Ejercicio 1.3
+ *
+ * Escriba un programa que escriba en la pantalla las notas de la asignatura
+ * "Programación". En la primera línea se escribirá el nombre de la asignatura.
+ * En las siguientes líneas se escribirán las notas de los dos parciales
+ * realizados poniendo la nota de cada uno en líneas distintas. En la última
+ * línea escribirá la nota final de la asignatura. Escriba lo que sea texto
+ * como un texto entre comillas dobles y lo que sea un número como un número.
+ *
+ * @author Unai de la O
+ */
+
+public class Ejercicio1_3 {
+  public static void main(String[] args) {
+    System.out.println("Notas de la asignatura \"Programación\"");
+    System.out.print("Primer parcial: ");
+    System.out.println("8.0");
+    System.out.print("Segundo parcial: ");
+    System.out.println("9.0");
+    System.out.print("Nota final: ");
+    System.out.println("8.5");
+  }
+}
