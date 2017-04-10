3365499753b63e787cca023a62f14206bf7a28e9
unaidelao <unai.delao@gmail.com>
2017-04-10 14:33:44 +0200

    Add Ejercicio 1.1

---
+++ java2-SchaumSeries/cap01/Ejercicio1_1.java
@@ -0,0 +1,17 @@
+/**
+ * Programación en Java 2, Serie Schaum
+ *
+ * Ejercicio 1.1
+ *
+ * Escriba un programa que escriba en la pantalla su nombre completo en una
+ * línea y en la línea siguiente su fecha de nacimiento.
+ *
+ * @author Unai de la O
+ */
+ 
+ public class Ejercicio1_1 {
+	 public static void main(String[] args) {
+		 System.out.println("Pepito Pérez");
+		 System.out.println("01/01/1980");
+	 }
+ }
