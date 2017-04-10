3a6b6cccc69014f31e88894ac5c80c9dc3db0c28
unaidelao <unai.delao@gmail.com>
2017-04-10 14:54:24 +0200

    Add Ejercicio 1.2

---
+++ java2-SchaumSeries/cap01/Ejercicio1_2.java
@@ -0,0 +1,21 @@
+/**
+ * Programación en Java 2, Serie Schaum
+ *
+ * Ejercicio 1.1
+ *
+ * Escriba un programa que escriba en la pantalla su nombre completo en una
+ * línea y en la línea siguiente su fecha de nacimiento. Para ello escriba una
+ * sentencia para escribir el nombre, otra sentencia para escribir su primer
+ * apellido y otra para escribir su segundo apellido.
+ *
+ * @author Unai de la O
+ */
+
+public class Ejercicio1_2{
+	public static void main(String[] args) {
+		System.out.print("Pepito ");
+		System.out.print("Pérez ");
+		System.out.println("Sánchez");
+		System.out.println("01/01/1980");
+	}
+}
