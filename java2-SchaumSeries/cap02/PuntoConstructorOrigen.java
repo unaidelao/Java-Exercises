787f4b0010aa3daf42c8868f4823a65373e2830d
unaidelao <unai.delao@gmail.com>
2017-05-09 11:52:52 +0200

    Add Ejercicio 2.10

---
+++ java2-SchaumSeries/cap02/PuntoConstructorOrigen.java
@@ -0,0 +1,15 @@
+/**
+ * Programación en Java 2, Serie Schaum
+ *
+ * Ejercicio 2.10
+ *
+ * Añada a la clase Punto un constructor sin parámetros que permita construir
+ * puntos en el origen de coordenadas.
+ *
+ * @author Unai de la O
+ */
+
+public Punto() {
+  this.x = 0;
+  this.y = 0;
+}
