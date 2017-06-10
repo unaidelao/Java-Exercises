b385765630de9f086c2beb45c5bf79faf5a3b30b
unaidelao <unai.delao@gmail.com>
2017-06-10 10:17:22 +0200

    Add Ejercicio 3.1

---
+++ java2-SchaumSeries/cap03/ConstantesMath.java
@@ -0,0 +1,20 @@
+/**
+ * Programación en Java 2 - Serie Schaum
+ *
+ * Ejercicio 3.1
+ *
+ * Escriba un programa que imprima en la pantalla las constantes de la clase
+ * Math.
+ *
+ * @author Unai de la O
+ */
+ 
+ public class ConstantesMath {
+   static double valorPI = Math.PI;
+   static double valorE = Math.E;
+   
+   public static void main(String[] args) {
+     System.out.println("El valor de PI es " + valorPI);
+     System.out.println("El valor de E es " + valorE);
+   }
+ }
