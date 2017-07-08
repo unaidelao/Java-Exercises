4aacac5642e41e32f7ad4cee61ee025b1d20fb28
unaidelao <unai.delao@gmail.com>
2017-07-08 09:19:32 +0200

    Add Ejercicio 4.5

---
+++ java2-SchaumSeries/cap04/OrdenaDosNumeros.java
@@ -0,0 +1,22 @@
+/**
+ * Programación en Java 2 - Serie Schaum
+ *
+ * Ejercicio 4.5
+ *
+ * Escriba un método, de nombre escribeOrdenadosDosNumeros, que reciba por
+ * parámetro dos números reales y los escriba de menor a mayor.
+ *
+ * @author Unai de la O
+ */
+
+ public class OrdenaDosNumeros {
+   public void escribeOrdenadosDosNumeros(int a, int b) {
+     System.out.println(a < b ? a + " " + b : b + " " + a);
+   }
+
+   // MAIN y prueba del programa
+   public static void main(String[] args) {
+     OrdenaDosNumeros ejemplo = new OrdenaDosNumeros();
+     ejemplo.escribeOrdenadosDosNumeros(89, 4);
+   }
+ }
