ea9bbd68b9f1ad643938727960e4e44ec3051e34
unaidelao <unai.delao@gmail.com>
2017-08-25 09:19:20 +0200

    Add Ejercicio 4.50

---
+++ java2-SchaumSeries/cap04/Corredor.java
@@ -0,0 +1,53 @@
+/**
+ * Programación en Java 2 - Serie Schaum
+ *
+ * Ejercicio 4.50
+ *
+ * Escriba una clase, de nombre Corredor, teniendo en cuenta las siguientes
+ * especificaciones:
+ *   a) La clase tendrá un atributo entero de nombre energía.
+ *   b) La clase tendrá un método constructor que reciba por parámetro una
+ *      cantidad de energía que asignará al atributo.
+ *   c) La clase tendrá un método, de nombre recargarEnergia, que recibirá por
+ *      parámetro una cantidad de energía que será sumada al atributo energía.
+ *   d) La clase tendrá un método, de nombre correr, que mostrará por pantalla
+ *      un mensaje y decrementará la energía en 10 unidades. Antes de proceder
+ *      al decremento, el método comprobará que la energía del corredor es
+ *      igual o superior a 10 unidades. Si no es así, el método lanzará una
+ *      excepción de tipo AgotadoExcepcion.
+ */
+
+ /*
+  * Clase para la excepción AgotadoExcepcion
+  */
+  class AgotadoExcepcion extends Exception {
+    public AgotadoExcepcion(String s) {
+      super (s);
+    }
+  }
+
+  /*
+   * Clase del corredor
+   */
+   public class Corredor {
+     int energia = 0;
+
+     // Constructor
+     public Corredor(int energia) {
+       this.energia = energia;
+     }
+
+     // Método para recargar la energía del corredor
+     public void recargarEnergia(int energia) {
+       this.energia += energia;
+     }
+
+     // Método para que el corredor corra
+     public void correr() throws AgotadoExcepcion {
+       if (energia < 10) {
+         throw new AgotadoExcepcion("Estoy agotado...(energia < 10)");
+       }
+       System.out.println("Estoy corriendo...(gasto 10 de energia)");
+       energia -= 10;
+     }
+   }
