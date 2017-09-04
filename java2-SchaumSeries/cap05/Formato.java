d70ef138ee850a9f62ed67eb4845030841adc1a3
unaidelao <unai.delao@gmail.com>
2017-09-04 15:41:47 +0200

    Add Ejercicio 5.9 parte 1 de 2 - enum Formato

---
+++ java2-SchaumSeries/cap05/Formato.java
@@ -0,0 +1,10 @@
+/**
+ * Programación en Java 2 - Serie Schaum
+ *
+ * Ejercicio 5.9 parte 1 de 2
+ *
+ * Enumerado con una serie de formatos, necesario para utilizar junto con la
+ * clase Multimedia del Ejercicio 5.9 parte 2 de 2, e incluso otras.
+ */
+
+ public enum Formato {WAV, MP3, MIDI, AVI, MOV, MPG, CDAUDIO, DVD}
