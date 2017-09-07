12978d5bf8f1aafe9bbf1c025d3b6873dd8772fb
unaidelao <unai.delao@gmail.com>
2017-09-07 08:07:12 +0200

    Add Ejercicio 5.13 parte 1 de 2

---
+++ java2-SchaumSeries/cap05/Genero.java
@@ -0,0 +1,11 @@
+/**
+ * Programación en Java 2 - Serie Schaum
+ *
+ * Ejercicio 5.13 parte 1 de 2
+ *
+ * Enumerado con una serie de géneros musicales, necesario para utilizar junto
+ * con diferentes clases.
+ */
+
+ // siempre podemos añadir nuevos géneros musicales
+ public enum Genero {ROCK, POP, SOUL, FUNKY, FLAMENCO, CLASICA, BLUES, JAZZ}
