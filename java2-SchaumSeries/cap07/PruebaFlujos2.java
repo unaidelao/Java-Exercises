30438973b0a3e5650ef141881bba8e01febfc688
unaidelao <unai.delao@gmail.com>
2019-05-03 00:47:43 +0200

    Se añade el contenido del Ejercicio 7.42.

--- java2-SchaumSeries/cap07/PruebaFlujos2.java
+++ java2-SchaumSeries/cap07/PruebaFlujos2.java
@@ -1,20 +1,35 @@
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * Programación en Java 2 - Serie Schaum
  *
- * Ejercicio 7.41
+ * === Ejercicio 7.41 ===
  * 
  * Se pide escribir una clase, de nombre PruebaFlujos2 donde se defina un método estático de
  * nombre almacenarColPuntosEnArchivo que reciba una colección de objetos de la clase Punto.
  * 
  * El método también recibirá por parámetro el nombre de un archivo para volcar sobre él la
  * información de los objetos "Punto" contenidos en la colección.
+ * 
+ * El método no capturará ninguna excepción.
+ * 
+ * === Ejercicio 7.42 ===
+ * 
+ * Añada a la clase anterior un método estático, de nombre obtenerArrayListDeArchivo, que reciba
+ * por parámetro el nombre de un archivo con los datos correspondientes a varios objetos de la
+ * clase Punto.
+ * 
+ * El método construirá y devolverá un objeto ArrayList<Punto> con los datos de los puntos leídos
+ * del archivo especificado.
  * 
  * El método no capturará ninguna excepción.
  */
@@ -24,6 +39,7 @@ public class PruebaFlujos2 {
 
     }
 
+    // Parte correspondiente al ejercicio 7.41.
     public static void almacenarColPuntosEnArchivo(Collection<Punto> puntos, String ruta) throws IOException {
         PrintWriter fichero = new PrintWriter(new BufferedWriter(new FileWriter(ruta)));
 
@@ -36,5 +52,20 @@ public class PruebaFlujos2 {
             fichero.println(temp.obtenerColor());
         }
         fichero.close();
+    }
+
+    // Parte correspondiente al ejercicio 7.42.
+    public static ArrayList<Punto> obtenerArrayListDeArchivo(String ruta) throws IOException, FileNotFoundException {
+        File fichero = new File(ruta);
+        Scanner sc = new Scanner(fichero);
+        ArrayList<Punto> alPuntos = new ArrayList<Punto>();
+
+        while (sc.hasNextInt()) {
+            Punto temp = new Punto(sc.nextInt(), sc.nextInt(), sc.next());
+            alPuntos.add(temp);
+        }
+        sc.close();
+
+        return alPuntos;
     }
 }
